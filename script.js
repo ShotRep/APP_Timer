acf689790a941dfa5af225a977d3065888674456
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-21 10:30:35 -0500

    starting on the animations.

--- script.js
+++ script.js
@@ -16,59 +16,9 @@ b.) reset internal timer to get ready for another run
 Break the problem down into small parts as much as needed.
 */
 
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput
-    this.startButton = startButton
-    this.pauseButton = pauseButton
-    if (callbacks) {
-      this.onStart = callbacks.onStart
-      this.onTick = callbacks.onTick
-      this.onComplete = callbacks.onComplete
-    }
-    //click event handler
-    this.startButton.addEventListener("click", this.start)
-    this.pauseButton.addEventListener("click", this.pause)
-  }
+/* Timer code was broken out into its own js file and is called first.
 
-  start = () => {
-    console.log("Timer Started!")
-    if (this.onStart) {
-      this.onStart()
-    }
-    this.tick()
-    this.interval = setInterval(this.tick, 1000)
-  }
-
-  pause = () => {
-    console.log("Timer Paused!")
-    clearInterval(this.interval)
-  }
-
-  tick = () => {
-    console.log("tick")
-    if (this.timeRemaining <= 0) {
-      this.pause()
-      if (this.onComplete) {
-        this.onComplete()
-      }
-    } else {
-      this.timeRemaining = this.timeRemaining - 1
-      if (this.onTick) {
-        this.onTick()
-      }
-    }
-  }
-  //getters and setters
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value)
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time
-  }
-}
-
+/* query selectors */
 const durationInput = document.querySelector("#duration")
 const startButton = document.querySelector("#start")
 const pauseButton = document.querySelector("#pause")
