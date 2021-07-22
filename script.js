5b33e0ac0afb541bb49b5b50593ae919ffc034b0
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-07-22 10:18:18 -0500

    Completed the styling in CSS

--- script.js
+++ script.js
@@ -4,15 +4,24 @@
 const durationInput = document.querySelector("#duration")
 const startButton = document.querySelector("#start")
 const pauseButton = document.querySelector("#pause")
+const circle = document.querySelector("circle")
 
+const perimeter = circle.getAttribute("r") * 2 * Math.PI
+circle.setAttribute("stroke-dasharray", perimeter)
+
+// let currentOffset = 0
+let duration
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log("onStart callback triggered")
+  onStart(totalDuration) {
+    duration = totalDuration
+    // console.log("onStart callback triggered")
   },
-  onTick() {
-    console.log("onTick callback triggered")
+  onTick(timeRemaining) {
+    // console.log("onTick callback triggered")
+    circle.setAttribute("stroke-dashoffset", perimeter * timeRemaining / duration - perimeter)
+   
   },
   onComplete() {
-    console.log("onComplete callback triggered")
+    // console.log("onComplete callback triggered")
   },
 })
--- timer.js
+++ timer.js
@@ -15,30 +15,30 @@ class Timer {
   }
 
   start = () => {
-    console.log("Timer Started!")
+    // console.log("Timer Started!")
     if (this.onStart) {
-      this.onStart()
+      this.onStart(this.timeRemaining)
     }
     this.tick()
-    this.interval = setInterval(this.tick, 1000)
+    this.interval = setInterval(this.tick, 10)
   }
 
   pause = () => {
-    console.log("Timer Paused!")
+    // console.log("Timer Paused!")
     clearInterval(this.interval)
   }
 
   tick = () => {
-    console.log("tick")
+    // console.log("tick")
     if (this.timeRemaining <= 0) {
       this.pause()
       if (this.onComplete) {
         this.onComplete()
       }
     } else {
-      this.timeRemaining = this.timeRemaining - 1
+      this.timeRemaining = this.timeRemaining - 0.01
       if (this.onTick) {
-        this.onTick()
+        this.onTick(this.timeRemaining)
       }
     }
   }
@@ -49,6 +49,6 @@ class Timer {
   }
 
   set timeRemaining(time) {
-    this.durationInput.value = time
+    this.durationInput.value = time.toFixed(2)
   }
 }
